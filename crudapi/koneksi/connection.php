eb3e15b2850745d9e4587584c26a25720d7631dd
Rafi <144548687+Abdul-is-os@users.noreply.github.com>
2025-11-21 17:55:57 +0700

    Add files via upload

---
+++ crudapi/koneksi/connection.php
@@ -0,0 +1,18 @@
+<?php
+$database_hostname = "localhost";
+$database_username = "root";
+$database_password = "";
+$database_name     = "utb_news";
+$database_port     = 3306;
+
+try {
+    $database_connection = new PDO(
+        "mysql:host=$database_hostname;port=$database_port;dbname=$database_name",
+        $database_username,
+        $database_password
+    );
+    
+} catch (PDOException $x) {
+    echo $x->getMessage();
+}
+?>
